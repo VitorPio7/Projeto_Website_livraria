9bcd17d2890e3933bf78dafc8e2d2edcfc12920a
Vitor Pio Vieira <106124241+VitorPio7@users.noreply.github.com>
2024-07-24 12:29:21 -0300

    hoje
    hoje

--- Projeto/index.js
+++ Projeto/index.js
@@ -1,6 +1,12 @@
 import express from "express";
 import bodyParser from "body-parser";
+import notifier from 'node-notifier';
+import path from 'path'
+import { dirname } from 'path';
+import { fileURLToPath } from "url";
+import { render } from "ejs";
 
+const __dirname = dirname(fileURLToPath(import.meta.url));
 const app = express();
 app.use(bodyParser.urlencoded({ extended: true }))
 app.use(express.static("public"))
@@ -16,6 +22,28 @@ app.get("/about", (req, res) => {
 })
 app.get("/FAQs", (req, res) => {
     res.render('Faqs.ejs')
+})
+app.post("/submit", (req, res) => {
+    res.render("Faqs.ejs", {
+        n: req.body.name,
+    })
+})
+app.get("/return", (req, res) => {
+    res.render("index.ejs")
+})
+
+app.post("/newsletter", (req, res) => {
+    res.render("index.ejs", {
+        email: req.body.email,
+        notification: notifier.notify({
+            title: 'Congradulation!',
+            message: "We've just received your email!",
+            icon: path.join(__dirname, 'public/img/logo.png'),
+            sound: true,
+            wait: true
+        })
+    });
+
 })
 
 app.listen(3000, () => {
